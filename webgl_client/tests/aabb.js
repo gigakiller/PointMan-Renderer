8ae5c53c6e0822c60237152f5589dd6479cffc2a
Uriah Baalke <uriahjb@gmail.com>
2013-12-07 18:48:23 -0500

    reduced number of vertices in webgl aabb test

--- webgl_client/tests/aabb.js
+++ webgl_client/tests/aabb.js
@@ -43,7 +43,6 @@
 		context.bindAttribLocation(program, positionLocation, "position");
 		u_modelViewPerspectiveLocation = context.getUniformLocation(program,"u_modelViewPerspective");
                 u_timeLocation = context.getUniformLocation(program, "u_time");
-                //u_color = context.getUniformLocation(program, "u_color");
 
         context.useProgram(program);
     })();
@@ -69,7 +68,7 @@
         var width = 0.1
         var lowCorner = [-width,-width,-width]
         var highCorner = [width,width,width]
-        // This works, now to make it more minimal 
+
         var positions = [
             // z-bottom
             lowCorner[0], lowCorner[1], lowCorner[2],
@@ -81,16 +80,6 @@
             lowCorner[0], highCorner[1], highCorner[2],
             highCorner[0], highCorner[1], highCorner[2],
             highCorner[0], lowCorner[1], highCorner[2],
-            // x-left
-            lowCorner[0], lowCorner[1], lowCorner[2],
-            lowCorner[0], lowCorner[1], highCorner[2],
-            lowCorner[0], highCorner[1], highCorner[2],
-            lowCorner[0], highCorner[1], lowCorner[2],
-            // x-right
-            highCorner[0], lowCorner[1], lowCorner[2],
-            highCorner[0], lowCorner[1], highCorner[2],
-            highCorner[0], highCorner[1], highCorner[2],
-            highCorner[0], highCorner[1], lowCorner[2]
             ];
         var indices = [
             // z-bottom
@@ -104,74 +93,16 @@
             5,6,
             6,7,
             // x-left
-            8,9,
-            8,11,
-            9,10,
-            10,11,
+            0,4,
+            0,1,
+            4,5,
+            5,1,
             // x-right
-            12,13,
-            12,15,
-            13,14,
-            14,15
+            3,7,
+            3,2,
+            7,6,
+            6,2
             ];
-       
-        /* 
-        var positions = new Float32Array( 3*4 );
-        var indices = new Uint16Array( 8 );
-        */
-
-        /*
-
-        var WIDTH_DIVISIONS = NUM_WIDTH_PTS - 1;
-        var HEIGHT_DIVISIONS = NUM_HEIGHT_PTS - 1;
-
-        var numberOfPositions = NUM_WIDTH_PTS * NUM_HEIGHT_PTS;
-
-        var positions = new Float32Array(2 * numberOfPositions);
-        var indices = new Uint16Array(2 * ((NUM_HEIGHT_PTS * (NUM_WIDTH_PTS - 1)) + (NUM_WIDTH_PTS * (NUM_HEIGHT_PTS - 1))));
-
-        var positionsIndex = 0;
-        var indicesIndex = 0;
-        var length;
-
-        for (var j = 0; j < NUM_WIDTH_PTS; ++j)
-        {
-            positions[positionsIndex++] = j /(NUM_WIDTH_PTS - 1);
-            positions[positionsIndex++] = 0.0;
-
-            if (j>=1)
-            {
-                length = positionsIndex / 2;
-                indices[indicesIndex++] = length - 2;
-                indices[indicesIndex++] = length - 1;
-            }
-        }
-
-        for (var i = 0; i < HEIGHT_DIVISIONS; ++i)
-        {
-             var v = (i + 1) / (NUM_HEIGHT_PTS - 1);
-             positions[positionsIndex++] = 0.0;
-             positions[positionsIndex++] = v;
-
-             length = (positionsIndex / 2);
-             indices[indicesIndex++] = length - 1;
-             indices[indicesIndex++] = length - 1 - NUM_WIDTH_PTS;
-
-             for (var k = 0; k < WIDTH_DIVISIONS; ++k)
-             {
-                 positions[positionsIndex++] = (k + 1) / (NUM_WIDTH_PTS - 1);
-                 positions[positionsIndex++] = v;
-
-                 length = positionsIndex / 2;
-                 var new_pt = length - 1;
-                 indices[indicesIndex++] = new_pt - 1;  // Previous side
-                 indices[indicesIndex++] = new_pt;
-
-                 indices[indicesIndex++] = new_pt - NUM_WIDTH_PTS;  // Previous bottom
-                 indices[indicesIndex++] = new_pt;
-             }
-        }
-        */
 
         uploadMesh(positions, heights, indices);
         numberOfIndices = indices.length;
