7d28e896ec6dfd36e8c2f4b23262a78b0a925194
Nathan Marshak <nmarshak@seas.upenn.edu>
2014-01-13 23:20:34 -0500

    Added utility for stripping unnecessary information from lines in the JavaScript console.

---
+++ util/strip_console_output.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python
+
+import sys
+
+if len(sys.argv) != 2:
+    print "Usage: strip_console_output file_name"
+    sys.exit(1)
+
+infile_name = sys.argv[1]
+print "Opening:", str(infile_name)
+
+in_f = open(infile_name, 'r')
+outfile_name = 'stripped_' + infile_name
+out_f = open(outfile_name, 'w')
+
+for line in in_f:
+        tokens = line.split()
+        #data = float(tokens[1])  # assume that the middle token is the value
+        out_f.write(tokens[1])
+        out_f.write('\n')
+
+in_f.close()
+out_f.close()
